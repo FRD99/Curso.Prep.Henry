1c154adcfe2b224fc1aa94e356607a92a5e02566
Fernando Hancco <fernandohanccoperez.18@gmail.com>
2022-05-18 18:20:41 -0500

    Terminado hasta JSV

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -164,8 +164,8 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí 
-  for(var i = 0 ; i < arreglo.length; i++){
-    var igualdad = arreglo[i] === arreglo[i,1];
+  for(var i = 0 ; i < arreglo.length; i++){ //[0,1,2,3...] i+1 se rompe 
+    var igualdad = arreglo[i] === arreglo[i,1]; //colocar arreglo.length-1
   }
   return igualdad;
 } 
